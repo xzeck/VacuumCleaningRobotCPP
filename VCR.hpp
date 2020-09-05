aee58f8c25dc3910547b1222c067b03fa8572ba7
Ajay <ajaypnair99@gmail.com>
2020-09-05 14:54:03 +0530

    Added a way to write the output as image, still not finished

--- VCR.hpp
+++ VCR.hpp
@@ -15,7 +15,39 @@ public:
 
 class diple {
 public:
-	int x, y;
+	unsigned int x, y;
+
+	diple(unsigned int x, unsigned int y) : x(x), y(y) {}
+	diple() : x(0), y(0) {}
+
+	diple& operator/= (const diple& rhs)
+	{
+		
+		x /= rhs.x; 
+		y /= rhs.y;
+
+		return *this;
+	}
+
+	diple& operator/ (const diple& rhs)
+	{
+		diple temp(*this);
+		temp /= rhs; 
+		return temp;
+	}
+};
+
+class Color
+{
+public:
+	unsigned char red; 
+	unsigned char blue; 
+	unsigned char green;
+
+	Color(unsigned char r, unsigned char b, unsigned char g) 
+		: red(r), blue(b), green(g) {}
+
+	Color() : red(0), blue(0), green(0) {}
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <sstream>
 #include "VCR.hpp"
 
 Grid** MAP;
@@ -51,20 +53,104 @@ void assign_grid_bits(GRIDBITS type, std::vector<diple>& grids )
 
 void print_grids(diple& size)
 {
-	for (int i = 0; i < size.x; i++)
+	for (uint i = 0; i < size.x; i++)
 	{
-		for (int j = 0; j < size.y; j++)
+		for (uint j = 0; j < size.y; j++)
 		{
 			std::cout << MAP[i][j].Type << " ";
 		}
 		std::cout << std::endl;
 	}
 }
+
+void Generate_image(std::string name, diple& resolution, diple& size)
+{
+	diple individual_grid_size;
+
+	individual_grid_size = resolution / size; 
+
+	uint** grid = new uint*[individual_grid_size.x];
+
+	for (uint i = 0; i < individual_grid_size.x; i++)
+	{
+		grid[i] = new uint[individual_grid_size.y];
+	}
+
+	std::ofstream imgOut(name);
+
+	if (!imgOut)
+	{
+		std::cout << "Could not open image file for writing" << std::endl;
+		exit(-1);
+	}
+
+	Color Unchecked(200, 0, 0);
+	Color Clean(0, 0, 200);
+	Color Obstacle(200, 0, 200);
+	Color Dirty(0, 200, 0);
+
+	uint red = 0;
+	uint green = 0 ;
+	uint blue = 0;
+ 
+
+	imgOut << "P3" << std::endl;
+	imgOut << resolution.x << " " << resolution.y << std::endl;
+	imgOut << 255 << std::endl;
+
+	for (uint x = 0; x < size.x; x++)
+	{
+		for (uint y = 0; y < size.y; y++)
+		{
+			for (uint j = 0; j < individual_grid_size.y; j++)
+			{
+				GRIDBITS Type = MAP[x][y].Type;
+				if (Type == unchecked)
+				{
+					red = Unchecked.red;
+					green = Unchecked.green;
+					blue = Unchecked.blue;
+
+				}
+				else if (Type == dirty)
+				{
+					red = Dirty.red;
+					green = Dirty.green;
+					blue = Dirty.blue;
+
+				}
+				else if (Type == clean)
+				{
+					red = Clean.red;
+					green = Clean.green;
+					blue = Clean.blue;
+				}
+				else if (Type == obstacle)
+				{
+					red = Obstacle.red;
+					green = Obstacle.green;
+					blue = Obstacle.blue;
+				}
+				for (uint i = 0; i < resolution.x; i++)
+				{
+						imgOut << red << " ";
+						imgOut << green << " ";
+						imgOut << blue << " ";
+				}
+
+			}
+		}
+	}
+	imgOut << std::endl;
+
+}
 int main()
 {
 	diple size; 
 	size.x = 3; 
 	size.y = 3; 
+
+	diple Resolution(300, 300);
 
 	std::vector<diple> dirty_grids;
 	std::vector<diple> obstacle_grids; 
@@ -92,6 +178,8 @@ int main()
 
 	print_grids(size);
 
+	Generate_image("initial.ppm", Resolution, size);
+
 	std::cout << std::endl;
 	std::cout << std::endl;
 
@@ -99,5 +187,7 @@ int main()
 
 	print_grids(size);
 
+	
+	Generate_image("final.ppm", Resolution, size);
     return 0;
 }
